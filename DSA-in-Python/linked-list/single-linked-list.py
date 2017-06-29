657c6f6d27baa2cf6e877c5408144138f9336d8a
AlabhyaVaibhav <alabhya.vaibhav1997@gmail.com>
2017-06-29 20:43:26 +0530

    single linked list

---
+++ DSA-in-Python/linked-list/single-linked-list.py
@@ -0,0 +1,78 @@
+	#Single-Linked-List
+	class Node:
+	 	"""The fundamental structure of a linked list"""
+	 	def __init__(self):
+	 		self.head = None
+	 		self.data = None
+	 		self.next = None
+	 	#This definition sets the data in the Node
+	 	def setData(self,data):
+	 		self.data = data
+	 	#This definition returns the data from the Node	
+	 	def getData(self):
+	 		return self.data
+	 	#This definition sets/points to the next Node in the linked list
+	 	def setNext(self):
+	 		self.next = next
+	 	#This definition returns the next field of the Node
+	 	def getNext(self):
+	 		return self.next
+	 	#This definition returns a TRUE if there exists a node next to the current one.
+	 	def hasNext(self):
+	 		return self.next != None
+	 	'''Basic operation in a Linked List 
+	 		1) Traversal 
+	 		2) Insertion of Nodes 
+	 		3) Deletion of Nodes
+	 	'''
+	 	#This definition finds the length of the linked list
+	 	def listLength(self):
+	 		current = self.head
+	 		count = 0 
+	 		while current != None:
+	 			count = count + 1
+	 			current = self.getNext()
+	 		return count
+	 	#Single Linked List Insertion
+	 	''' Here only one pointer has to be updated 
+			1) For an empty list 
+			 _ _ _ _ _
+	 		|data |   | -> none 
+              ^--HEAD
+            2 For a non empty list 
+			 _ _ _ _ _      _ _ _ _ _      
+	 		|data |   | -> | 51  |   | -> something cool stuff 
+                              ^--HEAD
+	 		 _ _ _ _ _      _ _ _ _ _      
+	 		|data |   | -> | 51  |   | -> something cool stuff 
+              ^--HEAD
+	 	'''
+	 	def insertAtBegining(self,data):
+	 		newNode = Node()
+	 		newNode.setData(data)
+	 		if self.listlength() == 0:
+	 			self.head = newNode
+	 		else
+	 			newNode.setNext(self.head)
+	 			self.head = newNode 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 		 
+
+
+
